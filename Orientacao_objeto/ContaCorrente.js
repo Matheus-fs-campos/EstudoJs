65dadc7afa0a8d77654d4e679c0f290c4f74a576
Matheus-fs-campos <76978616+Matheus-fs-campos@users.noreply.github.com>
2021-10-07 22:00:41 -0300

    Add files via upload
    voltando pro js - Herança

--- Orientacao_objeto/ContaCorrente.js
+++ Orientacao_objeto/ContaCorrente.js
@@ -1,56 +1,11 @@
-import { Cliente } from "./Cliente.js";
+import { Conta } from "./Conta.js";
 
-export class ContaCorrente {
+export class ContaCorrente extends Conta {
 
     static numeroDeContas=0;
-    agencia;
-
-
-    _cliente;
-    _saldo = 200; //inicialização
-
-
-    set cliente(novoValor){
-        if (novoValor instanceof Cliente){
-            this._cliente = novoValor;
-        }
-
-    }
-
-    get cliente(){
-        return this._cliente;
-    }
-
-    get saldo(){
-        return this._saldo;
-    }
-
-    constructor(cliente, agencia){
-        this.agencia = agencia;
-        this.cliente = cliente;
+   
+    constructor(saldoInicial,cliente, agencia){
+        super(saldoInicial, cliente, agencia); 
         ContaCorrente.numeroDeContas += 1;
     }   
-
-    sacar(valor){
-        if (this._saldo>=valor){
-            this._saldo-=valor;
-            return valor;
-        } else {
-            console.log("Sem _saldo");
-        }
-    }
-    
-    depositar(valor){
-        if (valor <=0){
-            console.log("Valor negativo")
-            return;
-        } else {
-            this._saldo+=valor;
-        }
-    }
-
-    transferir(valor, conta){
-        const valorSacado = this.sacar(valor);
-        conta.depositar(valor);
-    }
-};
+}
--- Orientacao_objeto/ContaPoupança.js
+++ Orientacao_objeto/ContaPoupança.js
@@ -1,7 +1,9 @@
-export class ContaPoupança {
+import { Conta } from "./Conta.js";
 
-    constructor (saldoInicial,cliente, agencia){
-        
+export class ContaPoupança extends Conta{
+
+    constructor(saldoInicial, cliente, agencia) {
+        super(saldoInicial, cliente, agencia);
     }
 
 }
