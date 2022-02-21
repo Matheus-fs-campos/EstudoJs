8aaec779e29ed21bf544a1b61ada4d62a7563019
Matheusfscampos <76978616+Matheus-fs-campos@users.noreply.github.com>
2022-02-21 15:14:16 -0300

    Interfaces
    Interfaces: usando propriedades e métodos

--- Orientacao_objeto/index.js
+++ Orientacao_objeto/index.js
@@ -13,13 +13,16 @@ import { ContaSalario } from "./Conta/ContaSalario.js";
 
 const diretor = new Diretor("Rodrigo",10000,12345678900);
 const gerente = new Gerente("Ricardo",5000,12345678901);
+const cliente = new Cliente("Lais",12345678902,"456")
 diretor.cadastrarSenha("123456789");
 gerente.cadastrarSenha("987654321");
+//cliente.cadastrarSenha("456");
 const estaLogadaDiretor = SistemaAutenticacao.login(diretor, "123456789");
-const estaLogadaGerente = SistemaAutenticacao.login(gerente, "987654321");
+//const estaLogadaGerente = SistemaAutenticacao.login(gerente, "987654321");
+const estaLogadaCliente = SistemaAutenticacao.login(cliente, "456");
 console.log("Diretor esta logado: " + estaLogadaDiretor);
-console.log("Gerente esta logado: " + estaLogadaGerente);
-
+//console.log("Gerente esta logado: " + estaLogadaGerente);
+console.log("Cliente esta logado: " + estaLogadaCliente);
 
 
 // -------------------------ÁREA DE CLIENTES------------------------
