df23ade5d6baa7be7aef3a47298a2c003420ef7a
Matheusfscampos <76978616+Matheus-fs-campos@users.noreply.github.com>
2022-03-14 22:49:04 -0300

    melhorando camada de validacao
    melhorando camada de validação com instanceof e entendendo o que é duck type

--- Orientacao_objeto/Conta/SistemaAutenticacao.js
+++ Orientacao_objeto/Conta/SistemaAutenticacao.js
@@ -1,7 +1,19 @@
-/* Ser autenticavel significa ter o metodo autenticar*/
+/* Ser autenticavel significa ter o metodo autenticar 
+Ducktype aparece muito em iguagens fracamente tipadas 
+
+*/
 
 export class SistemaAutenticacao{
     static login(autenticavel, senha){
-        return autenticavel.autenticar(senha);
+        if (SistemaAutenticacao.ehAutenticavel(autenticavel)){
+            return autenticavel.autenticar(senha);
+        }
+        return false;
+    }
+
+    static ehAutenticavel(autenticavel){
+        return "autenticar" in autenticavel && 
+        autenticavel.autenticar instanceof Function;//as alocações de memoria são feitas através do par chave-valor, ai podemos procurar um metodo através desse operador in
+
     }
 }
